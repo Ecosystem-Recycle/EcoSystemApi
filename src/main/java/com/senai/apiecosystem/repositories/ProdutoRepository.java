ca00cf21d021979e86dd56a457dc4ff9d6e979d6
Cleber-Fsantos <cleber.cfsantos@gmail.com>
2023-12-08 00:41:32 -0300

    fix ProdutoRepository

--- src/main/java/com/senai/apiecosystem/repositories/ProdutoRepository.java
+++ src/main/java/com/senai/apiecosystem/repositories/ProdutoRepository.java
@@ -2,15 +2,8 @@ package com.senai.apiecosystem.repositories;
 
 
 import com.senai.apiecosystem.models.ProdutoModel;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-<<<<<<< HEAD
-import org.springframework.data.jpa.repository.QueryRewriter;
-import org.springframework.data.repository.query.Param;
-=======
-
->>>>>>> 4a1eb62fa540bfb2800c0873500bc69ea4962cba
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,17 +11,7 @@ import java.util.UUID;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<ProdutoModel, UUID> {
-<<<<<<< HEAD
+
     @Query(value = "SELECT id, nome, quantidade, anuncio_id,categoria_id FROM produto WHERE anuncio_id = ?1", nativeQuery = true)
     List<ProdutoModel> findProdAnuncioById(UUID id);
 }
-
-
-=======
-
-
-}
-
-
-
->>>>>>> 4a1eb62fa540bfb2800c0873500bc69ea4962cba
